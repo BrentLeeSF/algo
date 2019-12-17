01c82738fbc75260bbc075262cef9b4f8d807de2
brent rucker <brent.rucker@gmail.com>
2019-12-17 09:20:36 -0800

    updating thread pool

--- MultiThreading/src/ThreadPoolTest.java
+++ MultiThreading/src/ThreadPoolTest.java
@@ -5,34 +5,30 @@ import java.util.Date;
 import java.util.concurrent.ExecutorService; 
 import java.util.concurrent.Executors; 
 
+
 // Task class to be executed (Step 1) 
-class Task implements Runnable 
-{ 
+class Task implements Runnable { 
 	private String name; 
 	
-	public Task(String s) 
-	{ 
+	public Task(String s)  { 
 		name = s; 
 	} 
 	
 	// Prints task name and sleeps for 1s 
 	// This Whole process is repeated 5 times 
-	public void run() 
-	{ 
-		try
-		{ 
-			for (int i = 0; i<=5; i++) 
-			{ 
-				if (i==0) 
-				{ 
+	public void run()  { 
+		
+		try {
+			
+			for (int i = 0; i<=5; i++) { 
+				if (i==0) { 
 					Date d = new Date(); 
 					SimpleDateFormat ft = new SimpleDateFormat("hh:mm:ss"); 
-					System.out.println("Initialization Time for"
-							+ " task name - "+ name +" = " +ft.format(d)); 
+					System.out.println("Initialization Time for"+ " task name - "+ name +" = " +ft.format(d)); 
 					//prints the initialization time for every task 
 				} 
-				else
-				{ 
+				
+				else { 
 					Date d = new Date(); 
 					SimpleDateFormat ft = new SimpleDateFormat("hh:mm:ss"); 
 					System.out.println("Executing Time for task name - "+ 
@@ -44,8 +40,7 @@ class Task implements Runnable
 			System.out.println(name+" complete"); 
 		} 
 		
-		catch(InterruptedException e) 
-		{ 
+		catch(InterruptedException e) { 
 			e.printStackTrace(); 
 		} 
 	} 
@@ -53,10 +48,13 @@ class Task implements Runnable
 
 
 public class ThreadPoolTest { 
+	
 	// Maximum number of threads in thread pool 
-	static final int MAX_T = 3;			 
+	static final int MAX_T = 3;	
+	
 
 	public static void main(String[] args) { 
+		
 		// creates five tasks 
 		Runnable r1 = new Task("task 1"); 
 		Runnable r2 = new Task("task 2"); 
