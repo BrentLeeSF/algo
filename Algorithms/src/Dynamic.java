2c970e984b48b928241c921d5cf4731cd307de91
brent rucker <brent.rucker@gmail.com>
2020-02-24 07:30:24 -0800

    update

--- Algorithms/src/Dynamic.java
+++ Algorithms/src/Dynamic.java
@@ -19,11 +19,11 @@ public class Dynamic {
 
 	/** fibonacci modified of (i-1)^2 + i-2 = */
 	public static int fibonacci(int num) {
-		
+
 		int[] fib = new int[num + 1];
 		fib[0] = 0;
 		fib[1] = 1;
-		
+
 		for (int i = 2; i <= num; i++) {
 			fib[i] = (fib[i - 1] * fib[i - 1]) + fib[i - 2];
 		}
