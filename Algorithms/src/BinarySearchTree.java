14be49c608d615b2e41a7aab5f9f7d3e01621ce4
brent rucker <brent.rucker@gmail.com>
2020-05-11 07:18:15 -0700

    update

--- Algorithms/src/BinarySearchTree.java
+++ Algorithms/src/BinarySearchTree.java
@@ -1,16 +1,5 @@
 import java.util.Stack;
 
-/*
------------------------------------------------------------------------------
-  date:                       12/16/2019
-  file:                       BinarySearchTree.java
------------------------------------------------------------------------------
-  compile:                    javac BinarySearchTree.java
-  run:                        java -cp . BinarySearchTree
-  debug:                      javac -Xlint BinarySearchTree.java
-  jvm version:                javap -verbose BinarySearchTree | grep "major"
------------------------------------------------------------------------------
-*/
 
 public class BinarySearchTree {
 
@@ -86,6 +75,7 @@ public class BinarySearchTree {
 	}
 
 	boolean check(BSTNode node, int min, int max) {
+		
 		if (node == null) {
 			return true;
 		}
@@ -98,104 +88,134 @@ public class BinarySearchTree {
 	}
 
 	public BSTNode insert(int num, BSTNode curr) {
+		
 		if (curr == null) {
 			return new BSTNode(num);
 		}
+		
 		if (num <= curr.data) {
 			curr.left = insert(num, curr.left);
 			return curr;
+			
 		} else {
 			curr.right = insert(num, curr.right);
 			return curr;
 		}
+		
 	}
 
 	public void printInOrder(BSTNode curr) {
+		
 		if (curr != null) {
 			printInOrder(curr.left);
 			System.out.print(curr.data + ", ");
 			printInOrder(curr.right);
 		}
+		
 	}
 
 	public void printPreOrder(BSTNode curr) {
+		
 		if (curr == null) {
 			return;
 		}
+		
 		System.out.print(curr.data + ", ");
 		printPostOrder(curr.left);
 		printPostOrder(curr.right);
 	}
 
 	public void printPostOrder(BSTNode curr) {
+		
 		if (curr == null) {
 			return;
 		}
+		
 		printPostOrder(curr.left);
 		printPostOrder(curr.right);
 		System.out.print(curr.data + ", ");
 	}
 
 	public void printInOrder(BSTNode curr, int indent) {
+		
 		if (curr != null) {
+			
 			for (int i = 0; i < indent; i++) {
 				System.out.print(" ");
 			}
+			
 			printInOrder(curr.left, indent + 1);
 			System.out.println(curr.data);
 			printInOrder(curr.right, indent + 1);
+			
 		}
 	}
 
 	public BSTNode findNode(BSTNode curr, int num) {
+		
 		if (curr.data == num) {
 			return curr;
 		}
+		
 		if (curr.data > num) {
 			return findNode(curr.left, num);
 			// return curr;
 		}
+		
 		return findNode(curr.right, num);
 		// return curr;
 	}
 
 	public int height(BSTNode thisRoot) {
-		if (thisRoot == null)
+		
+		if (thisRoot == null) {
 			return 0;
+		}
+			
 		return 1 + Math.max(height(thisRoot.left), height(thisRoot.right));
 	}
 
 	public boolean balance(BSTNode thisRoot) {
+		
 		int left = height(thisRoot.left);
 		int right = height(thisRoot.right);
 		System.out.println("Left height = " + left + ", right height = " + right);
 		return height(thisRoot.left) == height(thisRoot.right);
+		
 	}
 
 	public int smallestNode(BSTNode thisRoot) {
+		
 		if (thisRoot.left == null) {
 			return thisRoot.data;
 		}
+		
 		// return thisRoot.data = smallestNode(thisRoot.left);
 		return smallestNode(thisRoot.left);
 	}
 
 	public BSTNode LowestCommonAncestor(BSTNode root, int data1, int data2) {
+		
 		if (root == null) {
 			return null;
 		}
+		
 		if (data1 > data2) {
 			int temp = data2;
 			data2 = data1;
 			data1 = temp;
 		}
+		
 		while (root.data < data1 || root.data > data2) {
+			
 			if (root.data < data1) {
 				root = root.right;
+				
 			} else if (root.data > data2) {
 				root = root.left;
 			}
 		}
+		
 		return root;
 	}
 
@@ -214,7 +234,9 @@ public class BinarySearchTree {
 
 			} else if (thisNode.right == null) {
 				return thisNode.left;
+				
 			} else {
+				
 				if (thisNode.right.left == null) {
 					thisNode.data = thisNode.right.data;
 					thisNode.right = thisNode.right.right;
@@ -229,6 +251,7 @@ public class BinarySearchTree {
 		// if delete node data < || > data
 		if (thisNode.data > data) {
 			thisNode.left = delete(thisNode.left, data);
+			
 		} else if (thisNode.data < data) {
 			thisNode.right = delete(thisNode.right, data);
 		}
@@ -237,9 +260,11 @@ public class BinarySearchTree {
 	}
 
 	public int countNodes(BSTNode thisNode) {
+		
 		if (thisNode == null) {
 			return 0;
 		}
+		
 		return (countNodes(thisNode.left) + 1 + countNodes(thisNode.right));
 	}
 
@@ -277,17 +302,21 @@ public class BinarySearchTree {
 	 * http://stackoverflow.com/questions/31409989/what-is-the-best-approach-binary-search-tree-lowest-common-ancestor-using-onl
 	 */
 	public BSTNode lowestCommonAncestor(BSTNode root, int v1, int v2) {
+		
 		if (root == null) {
 			return null;
 		}
+		
 		/** If both v1 and v2 are smaller than root, then LCA lies in left */
 		if (root.data > v1 && root.data > v2) {
 			return lowestCommonAncestor(root.left, v1, v2);
 		}
+		
 		/** If both v1 and v2 are greater than root, then LCA lies in right */
 		if (root.data < v1 && root.data < v2) {
 			return lowestCommonAncestor(root.right, v1, v2);
 		}
+		
 		return root;
 	}
 	
