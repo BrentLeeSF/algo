fbb03860168d8c6a0e5d78c90d2e4146f6376adf
Brent Rucker <brent.rucker@gmail.com>
2021-01-13 10:20:07 -0800

    updating hackerrank

--- Hackerrank/src/DrawingBook.java
+++ Hackerrank/src/DrawingBook.java
@@ -1,6 +1,22 @@
-public class TheGridSearch {
-
+public class DrawingBook {
+// from - https://www.hackerrank.com/challenges/drawing-book/problem
+    int n = 5;
+    int p = 3;
     public static void main(String[] args) {
+        DrawingBook db = new DrawingBook();
+        System.out.println(db.drawBook(5,4));
+    }
 
+    public int drawBook(int n, int p) {
+        int shortestDistance = Math.min(n-p, p-0);
+        int count = 0;
+
+        while(shortestDistance >= 0) {
+            System.out.println("Shortest = "+shortestDistance+", count = "+count);
+            shortestDistance -= 2;
+            if(shortestDistance >= 0)
+                count++;
+        }
+        return count;
     }
 }
