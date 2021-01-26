f67efbf87461ff6c0b28089944085eed392f0d3b
Brent Rucker <brent.rucker@gmail.com>
2021-01-26 15:29:55 -0800

    updating leetcode

--- LeetCode/src/Medium.java
+++ LeetCode/src/Medium.java
@@ -33,7 +33,46 @@ public class Medium {
         int[] maxProductSubArray = {2,-3,5,1,-4};
         System.out.println("Max product of "+Arrays.toString(maxProductSubArray)+" = "+med.maximumProductSubArray(maxProductSubArray));
 
+        int nUniquePaths = 7, mUniquePaths = 3;
+        System.out.println("Unique Paths of "+nUniquePaths+", and "+mUniquePaths+" = "+med.uniquePaths(mUniquePaths,nUniquePaths));
+
+        int[] robberArray = {2,1,1,2};
+        System.out.println("Max of "+Arrays.toString(robberArray)+" = "+ med.houseRobber(robberArray));
     }
+
+    /* https://leetcode.com/problems/house-robber/ */
+    public int houseRobber(int[] nums) {
+        if(nums == null || nums.length == 0) { return 0; }
+        if(nums.length == 1) { return nums[0]; }
+        if(nums.length == 2) { return Math.max(nums[0],nums[1]); }
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for(int i = 2; i < dp.length; i++) {
+            dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
+        }
+        return dp[nums.length-1];
+    }
+
+    /* https://leetcode.com/problems/unique-paths/ */
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int j = 0; j < n; j++)
+            dp[0][j] = 1;
+        for(int i = 0;i < m; i++)
+            dp[i][0] = 1;
+        // add up all
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n ; j++){
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                System.out.print(dp[i][j]+", ");
+            }
+            System.out.println();
+        }
+        return dp[m-1][n-1];
+    }
+
 
     /** https://leetcode.com/problems/maximum-product-subarray/ */
 	public int maximumProductSubArray(int[] nums) {
