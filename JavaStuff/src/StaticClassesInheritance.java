6d23c8123ac641f0ac8cdd8a0ed843cbe0a84560
Brent Rucker <brent.rucker@gmail.com>
2021-02-17 07:30:49 -0800

    update

--- JavaStuff/src/StaticClassesInheritance.java
+++ JavaStuff/src/StaticClassesInheritance.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class StaticClassesInheritance {
+public class StaticClassesInheritance {
+
+    static class C1 {
+        int getValue1() {
+            return 1;
+        };
+    }
+    static class C2 extends C1 {
+        int getValue1() {
+            return super.getValue1() + 5;
+        }
+    }
+    static class C3 extends C2 {
+        @Override
+        public int getValue1() {
+            return 3 + super.getValue1();
+        }
+    }
+
+    public static void main(String[] args) {
+        C2 class1 = new C3();
+        System.out.println("Value1 = " + class1.getValue1());
+    }
 }
