3d6cc6f32fbdfefe730d2382768781168c19ea11
Brent Rucker <brent.rucker@gmail.com>
2021-02-18 11:21:18 -0800

    updating iterating through json objects

---
+++ JavaScript/jsonObjects.js
@@ -0,0 +1,97 @@
+
+const target = [
+	{
+		"id": "0001",
+		"type": "donut",
+		"name": "Cake",
+		"ppu": 0.55,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" },
+						{ "id": "1002", "type": "Chocolate" },
+						{ "id": "1003", "type": "Blueberry" },
+						{ "id": "1004", "type": "Devil's Food" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5005", "type": "Sugar" },
+				{ "id": "5007", "type": "Powdered Sugar" },
+				{ "id": "5006", "type": "Chocolate with Sprinkles" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	},
+	{
+		"id": "0002",
+		"type": "donut",
+		"name": "Raised",
+		"ppu": 0.55,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5005", "type": "Sugar" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	},
+	{
+		"id": "0003",
+		"type": "donut",
+		"name": "Old Fashioned",
+		"ppu": 0.55,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" },
+						{ "id": "1002", "type": "Chocolate" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	}
+];
+
+function deepIterator(target) {
+  if (typeof target === 'object') {
+  	if(target.id === "0003") {
+  		console.log();
+    	console.log("******0003******");
+    	console.log(target);
+    }
+    if(target.id > "5002") {
+    	console.log();
+    	console.log("*** LARGE ID ***");
+    	console.log(target);
+    }
+    if(target.type === "Glazed") {
+    	console.log();
+    	console.log("---Glazed---");
+    	console.log("Glazed = ",target.type,", id = ",target.id);
+    }
+    for (const key in target) {
+      deepIterator(target[key]);
+    }
+  }
+}
+console.log("Deep Iterator");
+console.log(deepIterator(target));
+
