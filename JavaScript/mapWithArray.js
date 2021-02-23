bee9b48f2e47c2620357d9223e6afcae6f370617
Brent Rucker <brent.rucker@gmail.com>
2021-02-22 17:56:31 -0800

    updating map with array

--- JavaScript/mapWithArray.js
+++ JavaScript/mapWithArray.js
@@ -35,3 +35,55 @@ function arrangeAnnagrams(anagrams) {
 }
 
 arrangeAnnagrams(anagrams);
+
+
+
+/**
+ * Return the user ids of the users that go in the order "A,B,C"
+ */
+
+func = (arr) => {
+  let map = new Map();
+  let newArr;
+
+  for(let i = 0; i < arr.length; i++) {
+    if(!map.has(arr[i].userId)) {
+      let newArr = [];
+      newArr.push(arr[i].action);
+      map.set(arr[i].userId, newArr);
+    } else {
+      newArr = map.get(arr[i].userId);
+      map.set(arr[i].userId, [arr[i].action, ...newArr]);
+    }
+  }
+
+  let alpha = ['A','B','C'];
+  let returnArr = [];
+  let thisArray = [];
+  add = true;
+
+  for (let [key, value] of map) {
+    thisArray = value;
+    for(let j = 0; j < alpha.length; j++) {
+      if(alpha[j] !== thisArray[thisArray.length-1-j]) {
+        add = false;
+      }
+    }
+    if(add) {
+      returnArr.push(key);
+    }
+    add = true;
+  }
+  return returnArr;
+}
+
+let firstArr = [
+  {userId: 1, action: 'A'}, {userId: 1, action: 'B'},{userId: 2, action: 'A'},
+  {userId: 2, action: 'B'}, {userId: 2, action: 'C'},{userId: 1, action: 'B'},
+  {userId: 3, action: 'A'}, {userId: 4, action: 'B'},{userId: 3, action: 'B'},
+  {userId: 4, action: 'B'}, {userId: 3, action: 'C'},{userId: 4, action: 'B'},
+  ];
+console.log();
+console.log("original array = ",firstArr)
+console.log(func(firstArr));
+
