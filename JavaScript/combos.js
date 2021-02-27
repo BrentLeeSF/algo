3a905129c08284c6952ce60f22dc2cb6e53b4491
Brent Rucker <brent.rucker@gmail.com>
2021-02-26 18:03:43 -0800

    update combos

--- JavaScript/combos.js
+++ JavaScript/combos.js
@@ -25,27 +25,76 @@ console.log("original array = ",originalComboArray, ", size = ",comboSize);
 console.log(combinationOfGivenArray(originalComboArray,comboSize));
 
 
+/* Triple Sub arrays that equal value */
+returnTriplets = (arr, sum) => {
+
+    arr.sort(function(a, b) {
+        return a - b;
+    });
+
+    let finalResults = [];
+
+    for(let i = 0; i < arr.length-2; i++) {
+
+        let k = sum-arr[i];
+        let low = i+1;
+        let high = arr.length-1;
+
+        while (low < high) {
+
+            if (arr[low] + arr[high] < k) {
+                low++;
+            }
+            else if (arr[low] + arr[high] > k) {
+                high--;
+            }
+   
+            // triplet with the given sum is found
+            else {
+                let newArr = [arr[i], arr[low], arr[high]];
+                finalResults.push(newArr);
+                low++;
+                high--;
+            }
+        }
+    }
+    return finalResults;
+}
+
+let tripletArray = [2, 7, 4, 0, 9, 5, 1, 3,9,-3 ];
+let tripletNum = 6;
+console.log("Returned triplets that = ",tripletNum," = ",returnTriplets(tripletArray, tripletNum));
+
+
 /* Return sub arrays that equal given number */
 const subArraySum = (nums, k) => {
-  const results = [];
-  for (let start = 0; start < nums.length; start++) {
-    let sum = 0;
-    for (let end = start; end <= nums.length; end++) {
-      sum += nums[end];
-      if (sum === k) {
-        results.push(nums.slice(start, end + 1));
-      }
+
+    const results = [];
+    for(let start = 0; start < nums.length; start++) {
+
+        let sum = 0;
+        for(let end = start; end <= nums.length; end++) {
+
+            sum += nums[end];
+            if(sum === k) {
+                results.push(nums.slice(start, end + 1));
+            }
+        }
     }
-  }
   return results;
 }
 
 let summArray = [4, 2, -3, -1, 0, 4];
 console.log();
-console.log(subArraySum(summArray, 0));
+let subArrayTotalOne = 0;
+console.log("Subarrays that add to ",subArrayTotalOne," = ",subArraySum(summArray, subArrayTotalOne));
 let secondArr = [6, 3, -1, -3, 4, -2, 2, 4, 6, -12, -7];
 console.log();
-console.log(subArraySum(secondArr, 0));
+console.log("Subarrays that add to ",subArrayTotalOne," = ",subArraySum(secondArr, subArrayTotalOne));
+let summArrayTwo = [0, 5, -7, 1, -4, 7, 6, 1, 4, 1, 10, -4];
+let subArraySumTotal = -3;
+console.log();
+console.log("Sub arrays that = ",subArraySumTotal," = ",subArraySum(summArrayTwo, subArraySumTotal));
 
 
 
