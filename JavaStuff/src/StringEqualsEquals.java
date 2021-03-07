839e50dd6c2900f97144b57fe2d97440017aebad
Brent Rucker <brent.rucker@gmail.com>
2021-03-07 09:23:08 -0400

    o

--- JavaStuff/src/StringEqualsEquals.java
+++ JavaStuff/src/StringEqualsEquals.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class StringEqualsEquals {
+
+public class StringEqualsEquals {
+
+    public static void main(String[] args) {
+        String s1 = "James Madison";
+        String s2 = "Madison";
+        String s3 = s2 = "James" + " " + s2;
+        System.out.println(s2);
+        System.out.println(s3);
+
+        if (s1 == s3) {
+            System.out.println("s1 and s3 are equal");
+        } else {
+            System.out.println("s1 and s3 are not equal");
+        }
+
+        if (s3 == s2) {
+            System.out.println("s3 and s2 are equal");
+        } else {
+            System.out.println("s3 and s2 are not equal");
+        }
+    }
 }
