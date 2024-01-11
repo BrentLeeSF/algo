ee8828e010dc06d536d05af600f84839cd340f60
Brent Rucker <brent.rucker@gmail.com>
2024-01-11 14:12:37 -0700

    balloon popping

--- Algorithms/src/Matricies.java
+++ Algorithms/src/Matricies.java
@@ -58,6 +58,42 @@ public class Matricies {
 		matricies.setToZero();
 
 		matricies.rotateClockwise(matty);
+
+
+		int[][] inOrderBalloons = {{1,2},{2,3},{3,4},{4,5}};
+		int[][] allDifferentBalloons = {{1,2},{3,4},{5,6},{7,8}};
+		int[][] outOfOrderBalloons = {{10,16},{2,8},{1,6},{7,12}};
+		matricies.minimumArrowsBurstBalloon(inOrderBalloons);
+		matricies.minimumArrowsBurstBalloon(allDifferentBalloons);
+		matricies.minimumArrowsBurstBalloon(outOfOrderBalloons);
+
+	}
+
+	/** https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/description/
+	 * There are some spherical balloons taped onto a flat wall that represents the XY-plane. The balloons are represented as a 2D integer array points where points[i] = [xstart, xend] denotes a balloon whose horizontal diameter stretches between xstart and xend. You do not know the exact y-coordinates of the balloons.
+	 * Arrows can be shot up directly vertically (in the positive y-direction) from different points along the x-axis. A balloon with xstart and xend is burst by an arrow shot at x if xstart <= x <= xend. There is no limit to the number of arrows that can be shot. A shot arrow keeps traveling up infinitely, bursting any balloons in its path.
+	 * Given the array points, return the minimum number of arrows that must be shot to burst all balloons.
+	 * Example 1:
+	 * Input: points = [[10,16],[2,8],[1,6],[7,12]]
+	 * Output: 2
+	 * Explanation: The balloons can be burst by 2 arrows:
+	 * - Shoot an arrow at x = 6, bursting the balloons [2,8] and [1,6].
+	 * - Shoot an arrow at x = 11, bursting the balloons [10,16] and [7,12].
+	 * */
+	public void minimumArrowsBurstBalloon(int[][] minArr) {
+		Arrays.sort(minArr, Comparator.comparingInt(o -> o[0]));
+		int count = minimumArrowsBurstBalloonHelper(minArr, minArr[0][1], 1);
+		System.out.println("COUNT = "+count);
+	}
+	public int minimumArrowsBurstBalloonHelper(int[][] minArr, int last, int count) {
+		for(int i = 1; i < minArr.length; i++) {
+			if(minArr[i][0] <= last) {
+				continue;
+			}
+			count++;
+			last = minArr[i][1];
+		}
+		return count;
 	}
 
 	public void printIntListMatricy(List<List<Integer>> thisList) {
